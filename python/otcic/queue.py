67563707c4ca2cbe14e31f58d968d7869c6bc692
v-karin <91729872+v-karin@users.noreply.github.com>
2023-01-13 17:46:19 +0000

    OTCIC Python: Setup queue.py
    DataDoubleQueue Class
    Very unfinished, WIP

---
+++ python/otcic/queue.py
@@ -0,0 +1,9 @@
+import time
+
+# 2 queues:
+# - aggergate queue
+# - real time queue
+# consider putting this to server-side processing?
+class DataDoubleQueue:
+    def __init__(self):
+        pass
