3f08d711a24e53023083dd196782f3ca65eb6c00
v-karin <91729872+v-karin@users.noreply.github.com>
2023-02-06 12:17:30 +0000

    Fixed http_server.py
    Properly import otcic, add function decorator, setup.

--- python/examples/http_server.py
+++ python/examples/http_server.py
@@ -3,17 +3,16 @@ from flask import Flask, request
 import sys
 import os
 sys.path.append('../python')
-from otcic import main
+import otcic
 
 app = Flask(__name__)
 
-main.setup()
-
 @app.route("/rolldice")
+@otcic.all_trace
 def roll_dice():
     return str(do_roll())
 
 def do_roll():
     return randint(1, 6)
 
-
+otcic.setup()
