4d9c326e5db7813feddc9da7871c16f8e62ed580
v-karin <91729872+v-karin@users.noreply.github.com>
2023-02-28 17:10:21 +0000

    Python: Added NVIDIA error guard to gpu_tracer.py

--- python/otcic/otcic/gpu_tracer.py
+++ python/otcic/otcic/gpu_tracer.py
@@ -9,22 +9,27 @@ class GPUTracer(DataDoubleQueue):
         super().__init__(process)
 
     def measure(self):
-        gpu_collection = gpustat.new_query()
-        process_gpus = []
-        gpu_list: list[GPUStat] = gpu_collection.gpus
-        for gpu in gpu_list:
-            for process in gpu.processes:
-                if process["pid"] == self.process.pid:
-                    process_gpus.append(process)
+        try:
+            gpu_collection = gpustat.new_query()
+            process_gpus = []
+            gpu_list: list[GPUStat] = gpu_collection.gpus
+            for gpu in gpu_list:
+                for process in gpu.processes:
+                    if process["pid"] == self.process.pid:
+                        process_gpus.append(process)
         
-        # incomplete and unsafe - need to do sanity check for "None"
-        memory = 0
-        for process_gpu in process_gpus:
-            memory += process_gpu["gpu_memory_usage"]
+            # incomplete and unsafe - need to do sanity check for "None"
+            memory = 0
+            for process_gpu in process_gpus:
+                memory += process_gpu["gpu_memory_usage"]
 
-        # add GPU usage? only cpu usage seen in docs and code
+            # add GPU usage? only cpu usage seen in docs and code
 
-        self.log(memory)
+            self.log(memory)
+        except Exception as e:
+            print("Unsupported GPU, use NVIDIA, returning 0")
+            print(e)
+            self.log(0)
 
     def collapse(self, start: int, interval: int):
         end = start + interval
