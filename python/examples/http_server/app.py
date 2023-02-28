0063e56f40bc40309cc7f885866d265928144ee4
v-karin <91729872+v-karin@users.noreply.github.com>
2023-02-28 19:04:13 +0000

    Make example HTTP Python app more resource intensive

--- python/examples/http_server/app.py
+++ python/examples/http_server/app.py
@@ -11,9 +11,34 @@ app = Flask(__name__)
 @app.route("/rolldice")
 @otcic.all_trace
 def roll_dice():
-    return str(do_roll())
+    return do_roll()
 
+table: list[list[int]] = []
 def do_roll():
-    return randint(1, 6)
+    value = randint(1, 3)
+    if value == 3 and len(table) < 17:
+        row = []
+        for i in range(2**16):
+            row.append(randint(1, 6))
+        table.append(row)
+        return "Table ADD: {}".format(row[0])
+
+    else:
+        r0 = randint(0, len(table) - 1)
+        row = table[r0]
+
+        if len(row) == 0:
+            table.pop(r0)
+            return "Table POP: row {}".format(r0)
+
+        else:
+            r1 = randint(0, len(row) - 1)
+            if value == 2:
+                v = row.pop(r1)
+                return "Table POP: {}".format(v)
+        
+            else:
+                row[r1] += 1
+                return "Table MOD: {}".format(row[r1])
 
 otcic.setup("flask-server")
