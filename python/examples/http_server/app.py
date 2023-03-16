0827e25b6ab8fc7065dd258b759afda2c0a75a99
v-karin <91729872+v-karin@users.noreply.github.com>
2023-03-16 11:57:08 +0000

    Python: Redone Flask App, with 3 routes: /add, /del, /mod

--- python/examples/http_server/app.py
+++ python/examples/http_server/app.py
@@ -1,9 +1,11 @@
 import random as r
 from flask import Flask, request
+from functools import wraps
 import sys
 import otcic
 import multiprocessing
 
+TABLE_MAX = 16
 ROW_LENGTH = 2**16
 CHUNK_SIZE = 2**4
 
@@ -14,15 +16,23 @@ app = Flask(__name__)
 
 table: Table = []
 filename = "table.txt"
-with open(filename, "w") as file:
-    file.write("Table\n")
 
+with open(filename, "w"): pass
+
+def overwrite_table():
+    with open(filename, "w") as file:
+        lines = ["Table"]
+        lines.extend([",".join([str(n) for n in row]) for row in table])
+        file.write("\n".join(lines))
 
 def expensive_function(_=None) -> float:
     return (r.random() * r.randint(1, 3)) / (r.random() + 1)
 
 def table_sizeof(table: Table) -> int:
     return sys.getsizeof(table) + sum(map(sys.getsizeof, table))
+
+def print_table():
+    print("Table size:", table_sizeof(table))
 
 
 @otcic.ram_trace
@@ -32,36 +42,47 @@ def make_row() -> Row:
         return pool.map(expensive_function, range(ROW_LENGTH), CHUNK_SIZE)
 
 
+def table_template(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print_table()
+        msg = func(*args, **kwargs)
+        overwrite_table()
+        print(msg)
+        return msg
+    return wrapper
+
+
+@app.route("/add")
 @otcic.ram_trace
-def do_roll():
-    print("Table size:", table_sizeof(table))
-    if len(table) < 16:
+@table_template
+def add_table() -> str:
+    if len(table) < TABLE_MAX:
         row = make_row()
-        with open(filename, "a") as file:
-            file.write(", ".join(["{}\n".format(item) for item in row]))
         table.append(row)
-        msg = "Table ADD: {}".format(row[0])
+        return "Table ADD: {}".format(row[0])
+    return "Table ADD: Max Limit Reached"
 
-    else:
-        d = r.randint(1, 16)
-        for _ in range(d):
-            table.pop(0)
-        msg = "Table DEL: {}".format(d)
-
-    for row in table:
-        for n in range(len(row)):
-            v = row[n]
-            v = (v + expensive_function()) % v
-
-    return msg
-
-
-@app.route("/")
+@app.route("/del")
 @otcic.ram_trace
-def roll_dice():
-    s = do_roll()
-    print(s)
-    return s
+@table_template
+def del_table() -> str:
+    if len(table) > 0:
+        d = r.randint(0, TABLE_MAX - 1)
+        table.pop(d)
+        return "Table DEL: {}".format(d)
+    return "Table DEL: Empty"
+
+@app.route("/mod")
+@otcic.ram_trace
+@table_template
+def table_mod() -> str:
+    if len(table) > 0:
+        for row in table:
+            for n in range(len(row)):
+                row[n] = (row[n] + expensive_function()) % 16
+        return "Table MOD: {}".format(row[0])
+    return "Table MOD: Empty"
 
 
 otcic.setup("flask-server")
