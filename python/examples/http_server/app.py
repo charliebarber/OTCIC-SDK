4f0eb82a6535ea7868d4387ab9f3ecac925ee674
v-karin <91729872+v-karin@users.noreply.github.com>
2023-03-18 00:56:07 +0000

    Python Multiprocessing: 1st attempt

--- python/examples/http_server/app.py
+++ python/examples/http_server/app.py
@@ -1,88 +1,95 @@
 import random as r
-from flask import Flask, request
-from functools import wraps
-import sys
-import otcic
-#import multiprocessing
-
-TABLE_MAX = 16
-ROW_LENGTH = 2**16
-CHUNK_SIZE = 2**4
-
-Row = list[int]
-Table = list[Row]
-
-app = Flask(__name__)
-
-table: Table = []
-filename = "table.txt"
-
-with open(filename, "w"): pass
-
-def overwrite_table():
-    with open(filename, "w") as file:
-        lines = ["Table"]
-        lines.extend([",".join([str(n) for n in row]) for row in table])
-        file.write("\n".join(lines))
+import multiprocessing
 
 def expensive_function(_=None) -> float:
     return (r.random() * r.randint(1, 3)) / (r.random() + 1)
 
-def table_sizeof(table: Table) -> int:
-    return sys.getsizeof(table) + sum(map(sys.getsizeof, table))
+# hide shared memory from fork processes, stop otcic from re-importing
+# check for "app" instead of "__main__" because running from a flask command
+if __name__ == "app":
+    from functools import wraps
+    import sys
+    import otcic
+    from flask import Flask, request
+    import os
 
-def print_table():
-    print("Table size:", table_sizeof(table))
+    TABLE_MAX = 16
+    ROW_LENGTH = 2**16
+    CHUNK_SIZE = 2**4
 
+    Row = list[int]
+    Table = list[Row]
 
-@otcic.ram_trace
-def make_row() -> Row:
-    return list(map(expensive_function, range(ROW_LENGTH)))
-    # with multiprocessing.Pool() as pool:
-    #     return pool.map(expensive_function, range(ROW_LENGTH), CHUNK_SIZE)
+    app = Flask(__name__)
 
+    table: Table = []
+    filename = "table.txt"
 
-def table_template(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        print_table()
-        msg = func(*args, **kwargs)
-        overwrite_table()
-        print(msg)
-        return msg
-    return wrapper
+    with open(filename, "w"): pass
 
+    def overwrite_table():
+        with open(filename, "w") as file:
+            lines = ["Table"]
+            lines.extend([",".join([str(n) for n in row]) for row in table])
+            file.write("\n".join(lines))
 
-@app.route("/add")
-@otcic.ram_trace
-@table_template
-def add_table() -> str:
-    if len(table) < TABLE_MAX:
-        row = make_row()
-        table.append(row)
-        return "Table ADD: {}".format(row[0])
-    return "Table ADD: Max Limit Reached"
+    def table_sizeof(table: Table) -> int:
+        return sys.getsizeof(table) + sum(map(sys.getsizeof, table))
 
-@app.route("/del")
-@otcic.ram_trace
-@table_template
-def del_table() -> str:
-    if len(table) > 0:
-        d = r.randint(0, len(table) - 1)
-        table.pop(d)
-        return "Table DEL: {}".format(d)
-    return "Table DEL: Empty"
-
-@app.route("/mod")
-@otcic.ram_trace
-@table_template
-def table_mod() -> str:
-    if len(table) > 0:
-        for row in table:
-            for n in range(len(row)):
-                row[n] = (row[n] + expensive_function()) % 16
-        return "Table MOD: {}".format(row[0])
-    return "Table MOD: Empty"
+    def print_table():
+        print("Table size:", table_sizeof(table))
 
 
-otcic.setup("flask-server")
+    print("Make row function declared")
+    @otcic.ram_trace
+    def make_row() -> Row:
+        print("Making row - pre-pool, pid:", os.getpid())
+        # return list(map(expensive_function, range(ROW_LENGTH)))
+        with multiprocessing.Pool() as pool:
+            return pool.map(expensive_function, range(ROW_LENGTH), CHUNK_SIZE)
+
+
+    def table_template(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print_table()
+            msg = func(*args, **kwargs)
+            overwrite_table()
+            print(msg)
+            return msg
+        return wrapper
+
+
+    @app.route("/add")
+    @otcic.ram_trace
+    @table_template
+    def add_table() -> str:
+        if len(table) < TABLE_MAX:
+            row = make_row()
+            table.append(row)
+            return "Table ADD: {}".format(row[0])
+        return "Table ADD: Max Limit Reached"
+
+    @app.route("/del")
+    @otcic.ram_trace
+    @table_template
+    def del_table() -> str:
+        if len(table) > 0:
+            d = r.randint(0, len(table) - 1)
+            table.pop(d)
+            return "Table DEL: {}".format(d)
+        return "Table DEL: Empty"
+
+    @app.route("/mod")
+    @otcic.ram_trace
+    @table_template
+    def table_mod() -> str:
+        if len(table) > 0:
+            for row in table:
+                for n in range(len(row)):
+                    row[n] = (row[n] + expensive_function()) % 16
+            return "Table MOD: {}".format(row[0])
+        return "Table MOD: Empty"
+
+
+    otcic.setup("flask-server")
--- python/otcic/otcic/__init__.py
+++ python/otcic/otcic/__init__.py
@@ -1,146 +1,147 @@
-from functools import wraps
-from opentelemetry.metrics import (
-    Observation,
-    get_meter_provider,
-    set_meter_provider,
-)
-from opentelemetry.sdk.metrics import (
-    Counter,
-    MeterProvider
-)
-from opentelemetry.sdk.metrics.export import (
-    ConsoleMetricExporter,
-    PeriodicExportingMetricReader,
-    AggregationTemporality
-)
-from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
+from multiprocessing import parent_process
 
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-
-from .aggregate import AggregateModel
-# this is where all classes are going to be used and this specific module is imported
-
-from cpuinfo import get_cpu_info
-
-import requests
-url = "http://api:54321/api/apps"
-
-# delete this
-
-INTERVAL_S = 3
-aggregate = AggregateModel(INTERVAL_S)
-
-
-def ram_trace(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        aggregate.tracers["ram"].measure()
-        return func(*args, **kwargs)
-    return wrapper
-
-
-def setup(service_name):
-    info = get_cpu_info()
-    model = info.get('brand_raw', "Unknown")
-    # Send app name and language to API
-    appObject = {
-        'appName': service_name,
-        'language': "python",
-        'cpuModel': model
-    }
-    requests.post(url, appObject)
-
-    # OpenTelemetry exporter setup
-    resource = Resource(attributes={
-        SERVICE_NAME: service_name
-    })
-
-    temporality_delta = {Counter: AggregationTemporality.DELTA}
-
-    exporter = OTLPMetricExporter(
-        endpoint="http://collector:4317",
-        preferred_temporality=temporality_delta
+if parent_process() == None:
+    from functools import wraps
+    from opentelemetry.metrics import (
+        Observation,
+        get_meter_provider,
+        set_meter_provider,
     )
-
-    # exporter = ConsoleMetricExporter(
-    #     preferred_temporality=temporality_delta
-    # )
-
-    metric_reader = PeriodicExportingMetricReader(
-        exporter,
-        export_interval_millis=INTERVAL_S * 1000,
+    from opentelemetry.sdk.metrics import (
+        Counter,
+        MeterProvider
     )
-
-    meter_provider = MeterProvider(
-        resource=resource,
-        metric_readers=[metric_reader]
+    from opentelemetry.sdk.metrics.export import (
+        ConsoleMetricExporter,
+        PeriodicExportingMetricReader,
+        AggregationTemporality
     )
-    set_meter_provider(meter_provider)
+    from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
 
-    meter = get_meter_provider().get_meter("metric-meter")
+    from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
-    def cpu_gauge_func(options):
-        aggregate.measure()
-        metrics = aggregate.get_metrics()
-        aggregate_data = metrics["cpu"]
-        val = aggregate_data[len(aggregate_data)-1][2]
+    from .aggregate import AggregateModel
+    # this is where all classes are going to be used and this specific module is imported
 
-        yield Observation(val)
+    from cpuinfo import get_cpu_info
 
-    meter.create_observable_gauge(
-        "cpu_gauge",
-        callbacks=[cpu_gauge_func]
-    )
+    import requests
+    url = "http://api:54321/api/apps"
 
-    def ram_gauge_func(options):
-        aggregate.measure()
-        metrics = aggregate.get_metrics()
-        aggregate_data = metrics["ram"]
-        val = aggregate_data[len(aggregate_data)-1][2]
+    INTERVAL_S = 3
+    aggregate = AggregateModel(INTERVAL_S)
 
-        yield Observation(val)
 
-    meter.create_observable_gauge(
-        "ram_gauge",
-        callbacks=[ram_gauge_func]
-    )
+    def ram_trace(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            aggregate.tracers["ram"].measure()
+            return func(*args, **kwargs)
+        return wrapper
 
-    def disk_gauge_func(options):
-        aggregate.measure()
-        metrics = aggregate.get_metrics()
-        aggregate_data = metrics["disk"]
-        pair = aggregate_data[len(aggregate_data)-1][2]
-        val = pair[0] + pair[1]
 
-        yield Observation(val)
+    def setup(service_name):
+        info = get_cpu_info()
+        model = info.get('brand_raw', "Unknown")
+        # Send app name and language to API
+        appObject = {
+            'appName': service_name,
+            'language': "python",
+            'cpuModel': model
+        }
+        requests.post(url, appObject)
 
-    meter.create_observable_gauge(
-        "disk_gauge",
-        callbacks=[disk_gauge_func]
-    )
+        # OpenTelemetry exporter setup
+        resource = Resource(attributes={
+            SERVICE_NAME: service_name
+        })
 
-    def gpu_gauge_func(options):
-        aggregate.measure()
-        metrics = aggregate.get_metrics()
-        aggregate_data = metrics["gpu"]
-        val = aggregate_data[len(aggregate_data)-1][2]
+        temporality_delta = {Counter: AggregationTemporality.DELTA}
 
-        yield Observation(val)
+        exporter = OTLPMetricExporter(
+            endpoint="http://collector:4317",
+            preferred_temporality=temporality_delta
+        )
 
-    meter.create_observable_gauge(
-        "gpu_gauge",
-        callbacks=[gpu_gauge_func]
-    )
+        # exporter = ConsoleMetricExporter(
+        #     preferred_temporality=temporality_delta
+        # )
 
-    def vram_gauge_func(options):
-        aggregate.measure()
-        metrics = aggregate.get_metrics()
-        aggregate_data = metrics["vram"]
-        val = aggregate_data[len(aggregate_data)-1][2]
+        metric_reader = PeriodicExportingMetricReader(
+            exporter,
+            export_interval_millis=INTERVAL_S * 1000,
+        )
 
-        yield Observation(val)
+        meter_provider = MeterProvider(
+            resource=resource,
+            metric_readers=[metric_reader]
+        )
+        set_meter_provider(meter_provider)
 
-    meter.create_observable_gauge(
-        "vram_gauge",
-        callbacks=[vram_gauge_func]
-    )
+        meter = get_meter_provider().get_meter("metric-meter")
+
+        def cpu_gauge_func(options):
+            aggregate.measure()
+            metrics = aggregate.get_metrics()
+            aggregate_data = metrics["cpu"]
+            val = aggregate_data[len(aggregate_data)-1][2]
+
+            yield Observation(val)
+
+        meter.create_observable_gauge(
+            "cpu_gauge",
+            callbacks=[cpu_gauge_func]
+        )
+
+        def ram_gauge_func(options):
+            aggregate.measure()
+            metrics = aggregate.get_metrics()
+            aggregate_data = metrics["ram"]
+            val = aggregate_data[len(aggregate_data)-1][2]
+
+            yield Observation(val)
+
+        meter.create_observable_gauge(
+            "ram_gauge",
+            callbacks=[ram_gauge_func]
+        )
+
+        def disk_gauge_func(options):
+            aggregate.measure()
+            metrics = aggregate.get_metrics()
+            aggregate_data = metrics["disk"]
+            pair = aggregate_data[len(aggregate_data)-1][2]
+            val = pair[0] + pair[1]
+
+            yield Observation(val)
+
+        meter.create_observable_gauge(
+            "disk_gauge",
+            callbacks=[disk_gauge_func]
+        )
+
+        def gpu_gauge_func(options):
+            aggregate.measure()
+            metrics = aggregate.get_metrics()
+            aggregate_data = metrics["gpu"]
+            val = aggregate_data[len(aggregate_data)-1][2]
+
+            yield Observation(val)
+
+        meter.create_observable_gauge(
+            "gpu_gauge",
+            callbacks=[gpu_gauge_func]
+        )
+
+        def vram_gauge_func(options):
+            aggregate.measure()
+            metrics = aggregate.get_metrics()
+            aggregate_data = metrics["vram"]
+            val = aggregate_data[len(aggregate_data)-1][2]
+
+            yield Observation(val)
+
+        meter.create_observable_gauge(
+            "vram_gauge",
+            callbacks=[vram_gauge_func]
+        )
