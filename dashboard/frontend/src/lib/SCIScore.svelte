7f164641d5bc5b9abee8f968489db22b44cdf23a
charliebarber <charlieb@protonmail.com>
2023-03-22 20:12:27 +0000

    Added conditional styling to SCI display:

---
+++ dashboard/frontend/src/lib/SCIScore.svelte
@@ -0,0 +1,54 @@
+<script>
+	export let score;
+
+	let scoreClass = 'good';
+	if (score > 300) {
+		scoreClass = 'bad';
+	} else if (score > 200) {
+		scoreClass = 'medium';
+	} else {
+		scoreClass = 'good';
+	}
+</script>
+
+<div>
+	<div class="scoreSpan">
+		<strong>SCI Score:</strong>
+		{#if score > 300}
+			<button class="bad">{score}</button>
+		{:else if score > 200}
+			<button class="medium">{score}</button>
+		{:else}
+			<button class="good">{score}</button>
+		{/if}
+	</div>
+</div>
+
+<style>
+	.scoreSpan {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+		font-size: 1.5rem;
+		margin-bottom: 0px;
+	}
+
+	button {
+		font-size: 1.5rem;
+		width: auto;
+		margin-bottom: 0px;
+	}
+
+	.good {
+		background-color: #40ff00;
+	}
+
+	.medium {
+		background-color: #ffbf00;
+	}
+
+	.bad {
+		background-color: #ff0040;
+	}
+</style>
