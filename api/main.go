0068d1ea44c7c93997813fb638c5f16b4018f109
charliebarber <charlieb@protonmail.com>
2023-03-23 16:48:06 +0000

    Moved API to its own folder at root

--- api/main.go
+++ api/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
-	"otcic/api/handlers"
+	"github.com/charliebarber/OTCIC-SDK/api/handlers"
 )
 
 func main() {
