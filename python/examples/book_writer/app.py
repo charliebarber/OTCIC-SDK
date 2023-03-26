798cc253c759369017f065fe8974dc70bf12ca74
v-karin <91729872+v-karin@users.noreply.github.com>
2023-03-26 19:04:01 +0100

    Python: fixed BookWriter app to run without errors

--- python/examples/book_writer/app.py
+++ python/examples/book_writer/app.py
@@ -7,15 +7,15 @@ import otcic
 
 # word syllable count
 SYL_MIN = 1
-SYL_MAX = 6
+SYL_MAX = 2 #6
 
 # sentence word count
-WORD_MIN = 4
-WORD_MAX = 8
+WORD_MIN = 1 #4
+WORD_MAX = 2 #8
 
 # book sentence/line count
-LINES_MIN = 40
-LINES_MAX = 400
+LINES_MIN = 2 #40
+LINES_MAX = 3 #400
 
 BOOKS_MAX = 32
 INTERVAL = 100
@@ -37,38 +37,34 @@ def make_word() -> str:
         ])
 
 @otcic.ram_trace
-def make_sentence() -> str:
-    return "".join([
-        " ".join([
-            make_word()
-            for i in range(r.randint(WORD_MIN, WORD_MAX))
-            ]),
-        ". "
-    ])
+def make_sentence(words: list | None = None) -> str:
+    if not words:
+        words = [make_word() for i in range(r.randint(WORD_MIN, WORD_MAX))]
+    return "".join([" ".join(words), ". "])
 
 
 
 @otcic.ram_trace
 def read_book(name: str) -> list[list[str]]:
     book = []
-    with open(name + ".txt") as file:
-        book = [[word for word in line.strip(".\n").split(" ")] for line in file]
+    with open("books/" + name + ".txt") as file:
+        book = [[word for word in line.strip().strip(".\n").split(" ")] for line in file]
+
     return book
 
 @otcic.ram_trace
-def write_book(name: str, book: list[list[str]]):
+def write_book(name: str, book: list[list[str]] = None):
     if not book:
-        book = "".join([
-            make_sentence()
-            for i in range(r.randint(LINES_MIN, LINES_MAX))
-        ])
+        book_lines = [make_sentence() for i in range(r.randint(LINES_MIN, LINES_MAX))]
+    else:
+        book_lines = [make_sentence(line) for line in book]
+    book_str2 = "".join(book_lines)
 
-    with open(name + ".txt", "w") as file:
-        file.write(book)
+    with open("books/" + name + ".txt", "w") as file:
+        file.write(book_str2)
 
 
 
-book_count = 0
 books = []
 
 def get_book() -> str:
@@ -76,19 +72,20 @@ def get_book() -> str:
 
 @otcic.ram_trace
 def main():
-    if book_count < BOOKS_MAX:
-        for i in range(r.randint(1, BOOKS_MAX - book_count)):
+    if len(books) < BOOKS_MAX:
+        for i in range(r.randint(1, BOOKS_MAX - len(books))):
             name = make_word()
             while name in books:
                 name = make_word()
             write_book(name)
-            book_count += 1
+            books.append(name)
     
     modified_books = []
-    for i in range(r.randint(1, book_count)):
+    for i in range(r.randint(1, len(books))):
         selected = get_book()
-        while selected in books:
-            selected = get_book
+        while selected in modified_books:
+            selected = get_book()
+        modified_books.append(selected)
 
         book_content = read_book(selected)
         for i in range(len(book_content)):
@@ -100,19 +97,23 @@ def main():
 
 
 def startup():
+    print("Book Writer Startup")
     filenames = []
     for path, dirs, files in os.walk("/books"):
         filenames = files
         break
-    book_count = len(filenames)
+
     for filename in filenames:
         books.append(filename.strip(".txt"))
 
     count = 1
     while True:
+        print("Book Writer Main Loop:", count)
         main()
+        time.sleep(0.1)
         count = (count + 1) % INTERVAL
         if count == 0:
+            print("Book Writer Waiting...")
             time.sleep(10)
 
 
